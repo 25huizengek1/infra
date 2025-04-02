79f41cd39f95da8d51eeb831ff659377429c461d
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2025-04-02 21:47:54 +0200

    make users immutable

--- nixos.nix
+++ nixos.nix
@@ -40,9 +40,15 @@
 
   services.openssh.enable = true;
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKO4+0nbySi9L5GSXTExGCWdkZBqi5WEqYB9fr4LwKyh bart@bart-laptop"
-  ];
+  users = {
+    users = {
+      root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKO4+0nbySi9L5GSXTExGCWdkZBqi5WEqYB9fr4LwKyh bart@bart-laptop"
+      ];
+    };
+
+    mutableUsers = false;
+  };
 
   environment.systemPackages = with pkgs; [
     curl
