cecaf03e90f6f59011e1ddd38365b9ddbd401996
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2025-04-06 08:45:37 +0200

    fix ambiguity

--- modules/android.nix
+++ modules/android.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 
+let
+  android = pkgs.androidenv.composeAndroidPackages { };
+in
 {
   environment.systemPackages = [
     pkgs.fdroidserver
-    pkgs.androidenv.composeAndroidPackages { }.all
+    android.androidsdk
   ];
 
   # TODO: systemd service for fdroid, maybe add Vagrant (as F-Droid recommends), or put it into an (OCI) container
