46675e6c15ee12fb900b3fcd3280c9713480c3c8
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-13 16:02:32 +0200

    Revert "Revert "Ja jonge die codebase van jou slaat helemaal nergens op""

---
+++ parts/treefmt.nix
@@ -0,0 +1,13 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.treefmt.flakeModule ];
+  perSystem.treefmt = {
+    programs.nixfmt.enable = true;
+    programs.deadnix = {
+      enable = true;
+      no-lambda-arg = true;
+      no-lambda-pattern-names = true;
+      no-underscore = true;
+    };
+  };
+}
