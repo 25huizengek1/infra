2102ff35081a2a6161cd2954df29da43d42962e3
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2025-08-19 21:03:24 +0000

    add laptop openssh sk key

--- machines/bart-server.nix
+++ machines/bart-server.nix
@@ -71,6 +71,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKO4+0nbySi9L5GSXTExGCWdkZBqi5WEqYB9fr4LwKyh bart@bart-laptop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdc+Tbt0d+pHMYrDjrT3Ui09NV38T3bFWk/OMEL4Dp6 u0_a374@bart-phone"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJ38XOn6VETxKPzT5SS1s3GexJmUV4P9aTNSe71DpFW bart@bart-pc"
+    "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBG012Fvtbxykbn9hjOKRTe1ZK0ZksMK1j/ZnVrYqzuADZDuYCdGH5TB5znV+NbJuBmRuAWerBLr/rMTpY4frST4AAAAEc3NoOg== barto@bart-laptop"
   ];
 
   environment.systemPackages = with pkgs; [
