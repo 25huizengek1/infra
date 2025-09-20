4772ea76966a28ec5bbeec6efcc046c1e4665b25
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2025-09-20 19:52:32 +0000

    chore: update everything

--- parts/pkgs.nix
+++ parts/pkgs.nix
@@ -6,29 +6,10 @@
     config.allowUnfree = true;
     overlays = [
       (_final: _prev: self.packages.${system})
-      (
-        final: prev:
-        let
-          headplanePkgs = inputs.headplane.overlays.default final prev;
-        in
-        headplanePkgs
-        // {
-          headplane = (
-            headplanePkgs.headplane.overrideAttrs (
-              finalPkg: prevPkg: {
-                pnpmDeps = final.pnpm_10.fetchDeps {
-                  inherit (finalPkg) pname version src;
-                  fetcherVersion = 2;
-                  hash = "sha256-CsrZjXl31sl/YRzpt/pyBtr4QKn1pLRHqu5hUcNVZbo=";
-                };
-              }
-            )
-          );
-        }
-      )
       (final: _prev: {
         jdk8 = final.temurin-bin-8;
       })
+      inputs.headplane.overlays.default
       inputs.copyparty.overlays.default
     ];
   };
