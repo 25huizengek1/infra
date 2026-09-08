8df898b7ac98b94cbaea94c11bd036d2f6ba2b40
Bart Oostveen <bart@bartoostveen.nl>
2026-09-08 02:09:17 +0200

    vector/renovate: allow renovate to pass json to changelog script

--- modules/infra/system-specific/vector/renovate.nix
+++ modules/infra/system-specific/vector/renovate.nix
@@ -38,7 +38,7 @@ in
       cacheTtlOverride.datasource-forgejo-tags = 0;
 
       allowedCommands = [
-        "^\\.forgejo/scripts/generate-renovate-changelog\\.sh(\\s+.*)?$"
+        "^\\.github/scripts/generate-changelog\\.sh\\s+'[^']+'$"
       ];
 
       packageRules = [
