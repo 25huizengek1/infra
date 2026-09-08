4798cb145ade5b0c1433e19e5f12e4c12942367a
Bart Oostveen <bart@bartoostveen.nl>
2026-09-08 02:34:26 +0200

    vector/renovate: correct changelog script path

--- modules/infra/system-specific/vector/renovate.nix
+++ modules/infra/system-specific/vector/renovate.nix
@@ -38,7 +38,7 @@ in
       cacheTtlOverride.datasource-forgejo-tags = 0;
 
       allowedCommands = [
-        "^bash\\s+\\.forgejo/scripts/generate-changelog\\.sh\\s+.*$"
+        "^bash\\s+\\.forgejo/scripts/generate-renovate-changelog\\.sh\\s+.*$"
       ];
 
       packageRules = [
